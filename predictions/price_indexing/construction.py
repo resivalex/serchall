d3ac15a934e1bd3dfde98ce558d370011a7fd4b7
Ivan Reshetnikov <resivalex@gmail.com>
2022-03-15 18:01:10 +0300

    Suppress prophet output. Add dumb model

--- predictions/price_indexing/construction.py
+++ predictions/price_indexing/construction.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime
 from prophet import Prophet
+import os
 
 
 def construct(df):
@@ -73,7 +74,8 @@ def extend_line(df, end_date):
     df.columns = ['ds', 'y']
     days_to_add = (end_date - df['ds'].max()).days
     model = Prophet()
-    model.fit(df)
+    with suppress_stdout_stderr():
+        model.fit(df)
     future_days = 366 * 3 # 3 years
     future_df = model.make_future_dataframe(periods=days_to_add + future_days)
     pred_df = model.predict(future_df)
@@ -81,3 +83,34 @@ def extend_line(df, end_date):
     result.columns = original_columns
 
     return result
+
+
+# https://github.com/facebook/prophet/issues/223#issuecomment-310497971
+class suppress_stdout_stderr(object):
+    '''
+    A context manager for doing a "deep suppression" of stdout and stderr in
+    Python, i.e. will suppress all print, even if the print originates in a
+    compiled C/Fortran sub-function.
+       This will not suppress raised exceptions, since exceptions are printed
+    to stderr just before a script exits, and after the context manager has
+    exited (at least, I think that is why it lets exceptions through).
+
+    '''
+    def __init__(self):
+        # Open a pair of null files
+        self.null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
+        # Save the actual stdout (1) and stderr (2) file descriptors.
+        self.save_fds = (os.dup(1), os.dup(2))
+
+    def __enter__(self):
+        # Assign the null pointers to stdout and stderr.
+        os.dup2(self.null_fds[0], 1)
+        os.dup2(self.null_fds[1], 2)
+
+    def __exit__(self, *_):
+        # Re-assign the real stdout/stderr back to (1) and (2)
+        os.dup2(self.save_fds[0], 1)
+        os.dup2(self.save_fds[1], 2)
+        # Close the null files
+        os.close(self.null_fds[0])
+        os.close(self.null_fds[1])
